6bd5e8f85299ee12145a76724058890e196fffc7
Adam Skołuda <vexhelel@gmail.com>
2020-04-05 16:39:09 +0200

    Add hoenix static buildpack

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :dobble_generator, DobbleGeneratorWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "dobble-generator.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
+  load_from_system_env: true,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
 
 # Do not print debug messages in production
 config :logger, level: :info
